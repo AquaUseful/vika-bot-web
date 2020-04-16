26d971327583c6a1c6e67402856ebeb6b8bc975a
RandomWP <l.vladik@outlook.com>
2020-04-16 15:59:48 +0300

    Navbar

--- app/modules/chats.py
+++ app/modules/chats.py
@@ -3,8 +3,22 @@ from app.utils import decorators
 
 blueprint = quart.Blueprint("chats", __name__)
 
+navbar_templ = (
+    {"text": "Dashboard",
+     "href": "#",
+     "active": False},
+    {"text": "Admin",
+     "href": "#",
+     "active": False},
+    {"text": "Info",
+     "href": "#",
+     "active": False}
+)
+
 
 @blueprint.route("/chat/dashboard")
 @decorators.need_token
 async def dashboard():
-    return await quart.render_template("chat_dashboard.html", title="Dashboard")
+    nav_items = navbar_templ
+    nav_items[0]["active"] = True
+    return await quart.render_template("chat_dashboard.html", title="Dashboard", nav_items=nav_items)
