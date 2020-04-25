6b20bae57ed381c3e471ea6bb1aba77712b8117c
RandomWP <l.vladik.m@gmail.com>
2020-04-26 02:38:05 +0300

    Fix unbans

--- app/static/js/users.js
+++ app/static/js/users.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $("div.alert").hide();
     $("button.btn-ban").click(banUser);
     $("button.btn-kick").click(kickUser);
+    $("button.btn-unban").click(unbanUser);
 });
 
 function showSpinner(user_id) {
@@ -19,8 +20,15 @@ function disableButtons(user_id) {
 
 function enableButtons(user_id) {
     $(`button[user_id=${user_id}]`).prop("disabled", false);
-}
+};
 
+function showAlert(user_id, timeout) {
+    $(`div.alert[user_id=${user_id}]`).slideDown("slow", function () {
+        setTimeout(function () {
+            $(`div.alert[user_id=${user_id}]`).slideUp("slow");
+        }, 2000);
+    });
+};
 
 function banUser() {
     user_id = $(this).attr("user_id");
@@ -33,10 +41,6 @@ function banUser() {
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({ user_id: parseInt(user_id) })
         });
-    jqxhr.always(function (data) {
-        enableButtons(user_id);
-        hideSpinner(user_id);
-    });
     jqxhr.done(function (data) {
         if (data.result) {
             window.location.reload();
@@ -61,14 +65,14 @@ function kickUser() {
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({ user_id: parseInt(user_id) })
         });
-    jqxhr.always(function (data) {
-        enableButtons(user_id);
-        hideSpinner(user_id);
-    });
     jqxhr.done(function (data) {
         if (data.result) {
             window.location.reload();
         } else {
+            hideSpinner(user_id);
+            disableButtons(user_id);
+            $(`div.alert[user_id=${user_id}]`).text("This user can not be kicked!");
+            showAlert(user_id, 1000);
         };
     });
     jqxhr.fail(function (jqxhr, textStatus) {
@@ -87,14 +91,12 @@ function unbanUser() {
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({ user_id: parseInt(user_id) })
         });
-    jqxhr.always(function (data) {
-        enableButtons(user_id);
-        hideSpinner(user_id);
-    });
     jqxhr.done(function (data) {
         if (data.result) {
             window.location.reload();
         } else {
+            hideSpinner(user_id);
+            disableButtons(user_id);
         };
     });
     jqxhr.fail(function (jqxhr, textStatus) {
