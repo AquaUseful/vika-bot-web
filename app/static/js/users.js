696b82bc82bc3aa773b10436a56e3264f16502ee
RandomWP <l.vladik@outlook.com>
2020-04-28 14:53:26 +0300

    Some name fixes

--- app/static/js/users.js
+++ app/static/js/users.js
@@ -1,3 +1,10 @@
+let ajaxParams = {
+    method: "POST",
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    data: ""
+}
+
 $(document).ready(function () {
     $("div.spinner-border").hide();
     $("div.alert").hide();
@@ -6,118 +13,120 @@ $(document).ready(function () {
     $("button.btn-unban").click(unbanUser);
 });
 
-function showSpinner(user_id) {
-    $(`div.spinner-border[user_id=${user_id}]`).show();
+function showSpinner(userID) {
+    $(`div.spinner-border[user_id=${userID}]`).show();
 };
 
-function hideSpinner(user_id) {
-    $(`div.spinner-border[user_id=${user_id}]`).hide();
+function hideSpinner(userID) {
+    $(`div.spinner-border[user_id=${userID}]`).hide();
 };
 
-function disableButtons(user_id) {
-    $(`button[user_id=${user_id}]`).prop("disabled", true);
+function disableButtons(userID) {
+    $(`button[user_id=${userID}]`).prop("disabled", true);
 };
 
-function enableButtons(user_id) {
-    $(`button[user_id=${user_id}]`).prop("disabled", false);
+function enableButtons(userID) {
+    $(`button[user_id=${userID}]`).prop("disabled", false);
 };
 
-function showAlert(user_id, timeout) {
-    $(`div.alert[user_id=${user_id}]`).slideDown("slow", function () {
+function showAlert(userID, timeout) {
+    $(`div.alert[user_id=${userID}]`).slideDown("slow", function () {
         setTimeout(function () {
-            $(`div.alert[user_id=${user_id}]`).slideUp("slow");
+            $(`div.alert[user_id=${userID}]`).slideUp("slow");
         }, timeout);
     });
 };
 
-function setAlertText(user_id, text) {
-    $(`div.alert[user_id=${user_id}]`).text(text);
+function setAlertText(userID, text) {
+    $(`div.alert[user_id=${userID}]`).text(text);
 };
 
 function banUser() {
-    user_id = $(this).attr("user_id");
-    disableButtons(user_id);
-    showSpinner(user_id);
-    let jqxhr = $.ajax("/chat/users/ban",
-        {
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({ user_id: parseInt(user_id) })
-        });
+    userID = $(this).attr("user_id");
+    disableButtons(userID);
+    showSpinner(userID);
+    let params = ajaxParams;
+    params.data = JSON.stringify({ user_id: parseInt(userID) })
+    let jqxhr = $.ajax("/chat/users/kick", params);
     jqxhr.done(function (data) {
         if (data.result) {
             window.location.reload();
         } else {
-            hideSpinner(user_id);
-            disableButtons(user_id);
-            setAlertText(user_id, "This user can not be banned!");
-            showAlert(user_id, 2000);
+            hideSpinner(userID);
+            disableButtons(userID);
+            setAlertText(userID, "This user can not be banned!");
+            showAlert(userID, 2000);
         };
     });
     jqxhr.fail(function (jqxhr, textStatus) {
-        hideSpinner(user_id);
-        enableButtons(user_id);
+        hideSpinner(userID);
+        enableButtons(userID);
         setAlertText(`Request failed! (${textStatus})`);
-        showAlert(user_id, 2000);
+        showAlert(userID, 2000);
     });
 
 };
 
 function kickUser() {
-    user_id = $(this).attr("user_id");
-    disableButtons(user_id);
-    showSpinner(user_id);
-    let jqxhr = $.ajax("/chat/users/kick",
-        {
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({ user_id: parseInt(user_id) })
-        });
+    userID = $(this).attr("user_id");
+    disableButtons(userID);
+    showSpinner(userID);
+    let params = ajaxParams
+    params.data = JSON.stringify({ user_id: parseInt(userID) })
+    let jqxhr = $.ajax("/chat/users/kick", params);
     jqxhr.done(function (data) {
         if (data.result) {
             window.location.reload();
         } else {
-            hideSpinner(user_id);
-            disableButtons(user_id);
-            setAlertText(user_id, "This user can not be kicked!");
-            showAlert(user_id, 2000);
+            hideSpinner(userID);
+            disableButtons(userID);
+            setAlertText(userID, "This user can not be kicked!");
+            showAlert(userID, 2000);
         };
     });
     jqxhr.fail(function (jqxhr, textStatus) {
-        hideSpinner(user_id);
-        enableButtons(user_id);
+        hideSpinner(userID);
+        enableButtons(userID);
         setAlertText(`Request failed! (${textStatus})`);
-        showAlert(user_id, 2000);
+        showAlert(userID, 2000);
     });
 };
 
 function unbanUser() {
-    user_id = $(this).attr("user_id");
-    disableButtons(user_id);
-    showSpinner(user_id);
-    let jqxhr = $.ajax("/chat/users/unban",
-        {
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({ user_id: parseInt(user_id) })
-        });
+    userID = $(this).attr("user_id");
+    disableButtons(userID);
+    showSpinner(userID);
+    let params = ajaxParams;
+    params.data = JSON.stringify({ user_id: parseInt(userID) })
+    let jqxhr = $.ajax("/chat/users/kick", params);
     jqxhr.done(function (data) {
         if (data.result) {
             window.location.reload();
         } else {
-            hideSpinner(user_id);
-            disableButtons(user_id);
-            setAlertText(user_id, "This user can not be unbanned!");
-            showAlert(user_id, 2000);
+            hideSpinner(userID);
+            disableButtons(userID);
+            setAlertText(userID, "This user can not be unbanned!");
+            showAlert(userID, 2000);
         };
     });
     jqxhr.fail(function (jqxhr, textStatus) {
-        hideSpinner(user_id);
-        enableButtons(user_id);
+        hideSpinner(userID);
+        enableButtons(userID);
         setAlertText(`Request failed! (${textStatus})`);
-        showAlert(user_id, 2000);
+        showAlert(userID, 2000);
     });
+};
+
+function promoteUser() {
+    userID = $(this).attr("user_id");
+    disableButtons(userID);
+    showSpinner(userID);
+    let params = ajaxParams;
+    params.data = JSON.stringify({ user_id: parseInt(userID) })
+    let jqxhr = $.ajax("/chat/users/kick", params);
+};
+
+function demoteUser() {
+    userID = $(this).attr("user_id");
+
 };
